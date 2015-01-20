a1176a657645a3d27b0480e3bacdc9c0101736c1
dawid <d.duniec@iceo.co>
2015-01-20 17:27:08 +0100

    clear

--- public/index.php
+++ public/index.php
@@ -1,216 +1,44 @@
 <?php
 
-echo "automatyczny push kurwa";
-
-
-use \Phalcon\Loader,
-    \Phalcon\DI\FactoryDefault,
-    \Phalcon\Db\Adapter\Pdo\Mysql,
-    \Phalcon\Mvc\Micro;
-
-require("config.php");
-
-$loader = new Loader();
-
-$loader->registerDirs(array(
-    __DIR__ . '/models/'
-))->register();
-
-$di = new FactoryDefault();
-$di->set('db', function(){
-    return new Mysql(array(
-        "host" => DB_HOST,
-        "username" => DB_USER,
-        "password" => DB_PASSWORD,
-        "dbname" => DB_NAME
-    ));
-});
-
-
-
-$app = new Micro($di);
-
-
-//Retrieves all robots
-$app->get('/api/robots', function() use ($app) {
-
-    $phql = "SELECT * FROM Robots ORDER BY name";
-    $robots = $app->modelsManager->executeQuery($phql);
-
-    $data = array();
-    foreach ($robots as $robot) {
-        $data[] = array(
-            'id' => $robot->id,
-            'name' => $robot->name,
-        );
-    }
-
-    echo json_encode($data);
-});
-
-//Searches for robots with $name in their name
-$app->get('/api/robots/search/{name}', function($name) use ($app) {
-
-    $phql = "SELECT * FROM Robots WHERE name LIKE :name: ORDER BY name";
-    $robots = $app->modelsManager->executeQuery($phql, array(
-        'name' => '%' . $name . '%'
-    ));
-
-    $data = array();
-    foreach ($robots as $robot) {
-        $data[] = array(
-            'id' => $robot->id,
-            'name' => $robot->name,
-        );
-    }
-
-    echo json_encode($data);
-
-});
-
-//Retrieves robots based on primary key
-$app->get('/api/robots/{id:[0-9]+}', function($id) use ($app) {
-
-    $phql = "SELECT * FROM Robots WHERE id = :id:";
-    $robot = $app->modelsManager->executeQuery($phql, array(
-        'id' => $id
-    ))->getFirst();
-
-    //Create a response
-    $response = new Phalcon\Http\Response();
-
-    if ($robot == false) {
-        $response->setJsonContent(array('status' => 'NOT-FOUND'));
-    } else {
-        $response->setJsonContent(array(
-            'status' => 'FOUND',
-            'data' => array(
-                'id' => $robot->id,
-                'name' => $robot->name
-            )
-        ));
-    }
-
-    return $response;
-});
-
-
-
-//Adds a new robot
-$app->post('/api/robots', function() use ($app) {
-
-
-    $robot = $app->request->getPost();
-
-
-    $phql = "INSERT INTO Robots (name, type, year) VALUES (:name:, :type:, :year:)";
-
-    $status = $app->modelsManager->executeQuery($phql, array(
-        'name' => $robot['name'],
-        'type' => $robot['type'],
-        'year' => $robot['year']
-    ));
-
-    //Create a response
-    $response = new Phalcon\Http\Response();
-
-    //Check if the insertion was successful
-    if ($status->success() == true) {
-
-        //Change the HTTP status
-        $response->setStatusCode(201, "Created");
-
-        $robot->id = $status->getModel()->id;
-
-        $response->setJsonContent(array('status' => 'OK', 'data' => $robot));
-
-    } else {
-
-        //Change the HTTP status
-        $response->setStatusCode(409, "Conflict");
-
-        //Send errors to the client
-        $errors = array();
-        foreach ($status->getMessages() as $message) {
-            $errors[] = $message->getMessage();
-        }
-
-        $response->setJsonContent(array('status' => 'ERROR', 'messages' => $errors));
-    }
-
-    return $response;
-});
-
-
-
-//Updates robots based on primary key
-$app->put('/api/robots/{id:[0-9]+}', function($id) use($app) {
-
-    $robot = $app->request->getPost();
-
-    $phql = "UPDATE Robots SET name = :name:, type = :type:, year = :year: WHERE id = :id:";
-    $status = $app->modelsManager->executeQuery($phql, array(
-        'id' => $id,
-        'name' => $robot['name'],
-        'type' => $robot['type'],
-        'year' => $robot['year']
-    ));
-
-    //Create a response
-    $response = new Phalcon\Http\Response();
-
-    //Check if the insertion was successful
-    if ($status->success() == true) {
-        $response->setJsonContent(array('status' => 'OK'));
-    } else {
-
-        //Change the HTTP status
-        $response->setStatusCode(409, "Conflict");
-
-        $errors = array();
-        foreach ($status->getMessages() as $message) {
-            $errors[] = $message->getMessage();
-        }
-
-        $response->setJsonContent(array('status' => 'ERROR', 'messages' => $errors));
-    }
-
-    return $response;
-});
-
-
-
-//Deletes robots based on primary key
-$app->delete('/api/robots/{id:[0-9]+}', function($id) use ($app) {
-
-    $phql = "DELETE FROM Robots WHERE id = :id:";
-    $status = $app->modelsManager->executeQuery($phql, array(
-        'id' => $id
-    ));
-
-    //Create a response
-    $response = new Phalcon\Http\Response();
-
-    if ($status->success() == true) {
-        $response->setJsonContent(array('status' => 'OK'));
-    } else {
-
-        //Change the HTTP status
-        $response->setStatusCode(409, "Conflict");
-
-        $errors = array();
-        foreach ($status->getMessages() as $message) {
-            $errors[] = $message->getMessage();
-        }
-
-        $response->setJsonContent(array('status' => 'ERROR', 'messages' => $errors));
-
-    }
-
-    return $response;
-});
-
-$app->handle();
-
-
-?>
+use Phalcon\Mvc\Micro;
+
+error_reporting(E_ALL);
+
+define('APP_PATH', realpath('..'));
+
+try {
+
+    /**
+     * Read the configuration
+     */
+    $config = include __DIR__ . "/../config/config.php";
+
+    /**
+     * Include Services
+     */
+    include APP_PATH . '/config/services.php';
+
+    /**
+     * Include Autoloader
+     */
+    include APP_PATH . '/config/loader.php';
+
+    /**
+     * Starting the application
+     * Assign service locator to the application
+     */
+    $app = new Micro($di);
+
+    /**
+     * Incude Application
+     */
+    include APP_PATH . '/app.php';
+
+    /**
+     * Handle the request
+     */
+    $app->handle();
+
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
