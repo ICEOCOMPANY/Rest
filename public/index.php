78c66397e76e60e891abde4e02cde0007f223819
dawid <d.duniec@iceo.co>
2015-01-22 16:51:42 +0100

    dodanie modelu token

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: Content-Type ");
 header("Access-Control-Allow-Headers: *");
+header("Access-Control-Max-Age: 86400");
 
 
 
